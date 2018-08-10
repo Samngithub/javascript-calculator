999a142f122bc9499fe18c9043e5c66f28085ce4
sam <shmuel.com.1@gmail.com>
2018-08-10 06:41:51 +0300

    shortened the code

--- calc.js
+++ calc.js
@@ -1,30 +1,24 @@
-clear=()=>{
-  $("input:text").val("");
-    console.log("r")
-  };
+$(document).ready(()=>{
+  clear=()=>{
+    $("input:text").val("");
+  }
   evaluate=()=>{
-  var va = eval($("input:text").val());
-     $("input:text").val(va);
+    var va = eval($("input:text").val());
+       $("input:text").val(va);
   }
-   sum =(eve)=>{
-     dir=$("input:text").val()+eve.target.value;
-    $("input:text").val(dir);
+  sum =(eve)=>{
+       dir=$("input:text").val()+eve.target.value;
+      $("input:text").val(dir);
   }
-  $("#1").click(sum);
-  $("#2").click(sum);
-  $("#3").click(sum);
-  $("#p").click(sum);
-  $("#4").click(sum);
-  $("#5").click(sum);
-  $("#6").click(sum);
-  $("#m").click(sum);
-  $("#7").click(sum);
-  $("#8").click(sum);
-  $("#9").click(sum);
-  $("#double").click(sum);
+  for(let i=0;i<10;i++){
+      $("#" + i).click(sum);
+  }
+    $("#p").click(sum);
+    $("#m").click(sum);
+    $("#double").click(sum);
+    $("#clear").click(clear);
+    $("#divide").click(sum);
+    $("#e").click(evaluate);  
+    
   
-  $("#clear").click(clear);
-  $("#0").click(sum);
-  $("#divide").click(sum);
-  $("#e").click(evaluate);
-  
+})
